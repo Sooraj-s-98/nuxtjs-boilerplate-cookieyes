6ef37c7e816fcc8392f77bc622b1b455fda0c085
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2023-11-01 11:02:04 +0000

    bb

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,13 @@ export default {
   nitro: {
     preset: 'vercel-edge',
   },
+  head() {
+    return {
+      script: [
+        {
+          src: "https://cdn-cookieyes.com/client_data/e22d63ca33a2ba42e27d4c88/script.js"
+        }
+      ]
+    }
+  }
 };
