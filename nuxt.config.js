3b1a31997094ff40a6c391ddb0838c895abb0d8e
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2023-11-01 16:26:13 +0530

    Create nuxt.config.js

---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+export default {
+  head() {
+    return {
+      script: [
+        {
+          src: "https://cdn-cookieyes.com/client_data/e22d63ca33a2ba42e27d4c88/script.js"
+        }
+      ],
+    }
